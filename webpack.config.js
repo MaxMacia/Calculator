f3afa9393cba6239adc485b91168e7eb8eb5cb73
Maxence Macia <maxence.macia@gmail.com>
2022-10-16 19:16:54 +0200

    Configuration de ESlint et réparation des erreurs

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,32 @@
-const path = require('path');
+/* global require */
+/* global module */
+/* global __dirname */
+
+
+const path = require("path");
 
 module.exports = {
-  mode: "production",
-  entry: {
-    polyfill: "babel-polyfill",
-    app: "./src/index.js",
-  },
-  output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist")
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"]
-          }
-        }
-      }
-    ]
-  }
+	mode: "production",
+	entry: {
+		polyfill: "babel-polyfill",
+		app: "./src/index.js",
+	},
+	output: {
+		filename: "[name].bundle.js",
+		path: path.resolve(__dirname, "dist")
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env"]
+					}
+				}
+			}
+		]
+	}
 };
